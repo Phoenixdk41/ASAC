896500db482a797ce7dfa6b9996684fa084ea9c7
Valentin <phoenix_x@hotmail.fr>
2019-05-09 17:33:03 +0200

    Ajout CSS header
    Création d'un fichier css propre au header et ajout d'un bootstrap modifié

--- Application/site/Design/header/header.php
+++ Application/site/Design/header/header.php
@@ -5,10 +5,23 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Acceuil</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="C:\wamp64\www\ASAC\Application\site\Design\bootstrap\css\bootstrap.css"/>
+        <link rel="stylesheet" type="text/css" media="screen" href="header.css"/>
+        <link rel="stylesheet" href="../bootstrap/css/bootstrap.css"/>
     </head>
-    <header style="background-color: lightgray;">
 
+    <header>
+        <nav class="navbar navbar-light bg-light">
+            <img id ="altranLogo" src="..\img\altranLogo.png"/>
+            <a href="" type="button" class="btn btn-info">Accueil</a>
+            <a href="" type="button" class="btn btn-info">Projets</a>
+            <a href="" type="button" class="btn btn-info">Consultants</a>
+            <a href="" type="button" class="btn btn-info">Clients</a>
+            <a href="" type="button" class="btn btn-info">Facturation</a>
+            <a href="" type="button" class="btn btn-info">Aide</a>
+            <a href="" type="button" class="btn btn-info">Connection</a>
+            
+    
+    </header>
 
 
 
