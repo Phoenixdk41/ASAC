c1e395c623b30c2e21be2c930cbf9ecba1ccde3c
Phoenixdk41 <phoenix_x@hotmail.fr>
2019-05-22 16:02:42 +0200

    modifications et ajout js

--- Application/maquette/accueil.php
+++ Application/maquette/accueil.php
@@ -7,40 +7,47 @@
         <title>Accueil- Asac</title>
     </head>
     
-    <body>
+  <body>
     <header>
         <nav class="navbar navbar-light bg-light">
-            <img id ="altranLogo" src="//maquette/Design/img/altranLogo.png"/>
+            <img id ="altranLogo" src="Design/img/altranLogo.png"/>
 
-            <a href="//maquette/accueil.php" type="button" class="btn btn-info">Accueil</a>
-            <a href="" type="button" class="btn btn-info">
+            <!-- Logo Altran -->
+            <button onclick = "afficherAccueil()" id="accueil" type="button" class="btn btn-info" >Accueil</button>
+
+            <button onclick = "afficherProjet()" id="projet" type="button" class="btn btn-info">
             <span class="glyphicon glyphicon-tags" aria-hidden="true"></span>
-            Projets  <span class="badge">4</span></a>
-            
-            <a href="" type="button" class="btn btn-info">
+            Projets  <span class="badge">4</span></button>
+           
+            <!-- Bouton Accueil -->
+            <button id="Astreintes" type="button" class="btn btn-info">
             <span class="glyphicon glyphicon-time" aria-hidden="true"></span> 
-            Astreintes</a>
+            Astreintes</button>
 
-            <a href="" type="button" class="btn btn-info">
+            <!-- Bouton Consultants -->
+            <button id="Consultants" type="button" class="btn btn-info">
             <span class="glyphicon glyphicon-signal" aria-hidden="true"></span>
-            Consultants</a>
+            Consultants</button>
 
-            <a href="" type="button" class="btn btn-info">
+            <!-- Bouton Clients -->
+            <button  id="clients" type="button" class="btn btn-info">
             <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-            
-            Clients</a>
-            <a href="" type="button" class="btn btn-info">
+            Clients</button>
+
+            <!-- Bouton Facturation -->
+            <button id="facturation" type="button" class="btn btn-info">
             <span class="glyphicon glyphicon-piggy-bank" aria-hidden="true"></span>
-            Facturation</a>
+            Facturation </button>
 
-            <a href="" type="button" class="btn btn-info">
+            <!-- Bouton Consultants -->
+            <button id="aide" type="button" class="btn btn-info">
             <span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>
-            Aide</a>
+            Aide</button>
 
-            <a href="//maquette/auth.php" type="button" class="btn btn-info">
+            <!-- Bouton Connexion -->
+            <button id="connection" type="button" class="btn btn-info">
             <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-            Connexion</a>
-
+            Connexion</button>
     </header>
 
     <!--Séparation du code, à fractionner plus tard-->
@@ -49,10 +56,11 @@
 
     <div>
         <p>
-        Bienvenue sur la page de l'application maquette, pour avoir une vision, merci de vous connecter.
+        Bienvenue sur la page de l'application maquette, pour avoir une vision complete depuis votre status, merci de vous connecter.
         </p>
     </div>
 
+<div id="accueilBody">
     <table class="table">
   <thead>
     <tr>
@@ -65,27 +73,53 @@
   <tbody>
     <tr>
       <th scope="row">Tenue à la charge</th>
-      <td>Mark</td>
-      <td>Otto</td>
+      <td>Les details de la descriptions sont à definir avec le client</td>
+      <td>
+      <div class="progress">
+<div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+<span>60% effectué </span>
+</div>
+</div>
+    </td>
       <td><span class="badge">4</span></td>
     </tr>
     <tr>
       <th scope="row">Convergence Drive</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <td>Les details de la descriptions sont à definir avec le client</td>
+      <td>
+      <div class="progress">
+<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em;">
+0%
+</div>
+</div> 
+      
+      </td>
+      <td>A demarrer</td>
     </tr>
     <tr>
       <th scope="row">Optimisation Décode</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <td>Les details de la descriptions sont à definir avec le client</td>
+      <td>
+      <div class="progress">
+<div class="progress-bar progress-bar-success progress-bar-striped.active role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+<span>100% - terminé (success)</span>
+</div>
+</div>
+      </td>
+      <td>-</td>
     </tr>
   </tbody>
 </table>
+</div>
+
+<div id = "projetBody">
+  <table class="table">
+
+  </table>
+  
+</div>
 
 
-           
     </body>
 </html>
 
@@ -104,3 +138,28 @@
         margin-right: 5px;
     }
     </style>
+
+<script type="text/javascript">
+
+function afficherAccueil() {
+var visible = document.getElementById("accueilBody");
+
+  if (visible.style.display === "none")
+  {
+    visible.style.display = "block";
+  }
+
+  else 
+  {
+    visible.style.display = "none";
+  }
+
+}
+
+function afficherProjet(){
+  document.getElementById("accueilBody").style.display = "none";
+  document.getElementById("projetBody").style.display = "block";
+}
+
+</script>
+
